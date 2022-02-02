f8428914b8ec9972d3c5ab5f8bd05172867daa33
Eric Van <ggclarence@yahoo.com>
2022-02-01 19:40:03 -0600

    commit tree a4b4b834eff144110c51544739b63e0ecf5e3cba

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solution in this file!
+const headquaters = 42
+function distanceFromHqInBlocks(pickupLocation){
+
+let distanceByblocks
+    distanceByblocks = Math.abs(pickupLocation - headquaters)
+return distanceByblocks
+ }
+
+ function distanceFromHqInFeet(pickupLocation){
+     return distanceFromHqInBlocks(pickupLocation)*264;
+}
+
+function distanceTravelledInFeet(start,destination){
+   return Math.abs(start - destination)*264;
+}
+
+function calculatesFarePrice(start, destination){
+let distance = distanceTravelledInFeet(start,destination)
+    if (distance <= 400)
+        return 0
+    else if (distance >= 400 && distance <= 2000)
+        return (distance-400) * .02
+    else if (distance >= 2000 && distance <=2500)
+        return 25
+    else if (distance >= 2500) 
+        return 'cannot travel that far'
+   
+}
